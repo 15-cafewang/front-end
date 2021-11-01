1ad39c29d83c488310fa3dce0a5ee2d258254109
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-01 12:15:06 +0900

    [feat]:DropDown 완성

---
+++ mycipe/src/hooks/useDetectOutsideClick.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+/**
+el: React.RefObject<HTMLDivElement> 
+initialState: boolean
+ */
+export const useDetectOutsideClick = (el, initialState) => {
+  const [isActive, setIsActive] = useState(initialState);
+
+  useEffect(() => {
+    const onClick = (e) => {
+      console.log(e);
+      if (el.current && !el.current.contains(e.target)) {
+        // ref 객체의 current, 즉 ref에 값이 잡히는 경우 (렌더 메서드 안에서 ref가 엘리먼트에게 전달되었을 때)
+
+        // ref 객체의 current요소가 e.target(클릭한 요소)를 포함하고 있지 않다면, 내부의 컴포넌트가 아닌 외부를 클릭한 경우
+
+        // setIsActive(!isActive) isActive를 반대로
+        setIsActive(!isActive);
+      }
+    };
+    if (isActive) {
+      window.addEventListener("click", onClick);
+    }
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, [isActive, el]);
+
+  return [isActive, setIsActive];
+};
--- mycipe/src/shared/App.js
+++ mycipe/src/shared/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../index.css";
+import DropDown from "./DropDown";
+import { useDetectOutsideClick } from "../hooks";
+import { ReactComponent as More } from "../assets/icon/more.svg";
 
 function App() {
-  return <div></div>;
+  //useRef로 변수를 관리하게 되면, 그 변수가 업데이트
+  const dropdownRef = useRef();
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const toggleActive = () => setIsActive(!isActive);
+  return (
+    <div>
+      <button onClick={toggleActive}>
+        <More />
+        <DropDown
+          _ref={dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
+        />
+      </button>
+    </div>
+  );
 }
 
 export default App;
