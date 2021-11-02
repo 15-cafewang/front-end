d7cf780ceb7b34e46fa9688161dce0709c1879bc
shrkdvy <shrkdvy123@gmail.com>
2021-11-02 22:29:28 +0900

    [feat] Header,BottomNav 추가

--- mycipe/src/shared/App.js
+++ mycipe/src/shared/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "../index.css";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+import Header from "./Header";
+import BottomNav from "./BottomNav";
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Route component={Header} />
+      <Route component={BottomNav} />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  max-width: 375px;
+  height: 780px;
+`;
 
 export default App;
