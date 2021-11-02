ce483aee98bcbddbb93377840504a7f270e35153
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-02 20:31:49 +0900

    [edit] : DropDown수정

--- mycipe/src/shared/App.js
+++ mycipe/src/shared/App.js
@@ -1,20 +1,8 @@
-import React, { useRef } from "react";
+import React from "react";
 import "../index.css";
-import DropDown from "./DropDown";
-import { useDetectOutsideClick } from "../hooks";
-import { ReactComponent as More } from "../assets/icon/more.svg";
 
 function App() {
-  //useRef로 변수를 관리하게 되면, 그 변수가 업데이트
-  const dropdownRef = useRef();
-  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
-  const toggleActive = () => setIsActive(!isActive);
-  return (
-    <div>
-      <More onClick={toggleActive} />
-      <DropDown _ref={dropdownRef} className={` ${isActive ? "active" : ""}`} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
