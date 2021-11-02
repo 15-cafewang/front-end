9dccda4b60c413e322e0ae533e33cbbffcdd3f41
박하린 <phr0210@naver.com>
2021-11-02 22:27:39 +0900

    [feat] : RecipeBoardWrite 페이지 라우터 연결

--- mycipe/src/shared/App.js
+++ mycipe/src/shared/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-import "../index.css";
+import styled from "styled-components";
+
+import { Route } from "react-router-dom";
+
+// pages
+import RecipeBoardWrite from "../pages/RecipeBoard/RecipeBoardWrite";
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Route path="/recipeboard/write" exact component={RecipeBoardWrite} />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  max-width: 375px;
+  height: 780px;
+  margin: 0 auto;
+`;
 
 export default App;
