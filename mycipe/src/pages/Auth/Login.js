fd1b6dcf37404947d7b77b018af6a03a6f6def1c
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-03 16:36:25 +0900

    [feat] 로그인,회원가입 페이지 추가(ing)

---
+++ mycipe/src/pages/Auth/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "../../elements";
+
+const Login = () => {
+  return (
+    <>
+      <Button>로그인</Button>
+    </>
+  );
+};
+
+export default Login;
---
+++ mycipe/src/pages/Auth/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <>
+      <div>test</div>
+    </>
+  );
+};
+
+export default Signup;
