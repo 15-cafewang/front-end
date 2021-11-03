e3cee18c0ef53e2b41c9f45640b6928ddf08694d
박하린 <phr0210@naver.com>
2021-11-03 23:46:35 +0900

    [feat] : 자유게시판 페이지 추가

---
+++ mycipe/src/pages/BulletinBoard/BulletinBoardWrite.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import BoardWrite from "../../components/Board/BoardWrite";
+
+const RecipeBoardWrite = () => {
+  return (
+    <>
+      <BoardWrite boardName="bulletinBoard" />
+    </>
+  );
+};
+
+export default RecipeBoardWrite;
