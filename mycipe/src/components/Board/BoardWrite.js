9eb319a26a5111487af3b4e1689c30e38a01799d
박하린 <phr0210@naver.com>
2021-11-03 23:51:45 +0900

    주석 추가

--- mycipe/src/components/Board/BoardWrite.js
+++ mycipe/src/components/Board/BoardWrite.js
@@ -1,22 +1,79 @@
 import React from "react";
 import styled from "styled-components";
 
+// shard components
 import ImageUpload from "../../shared/ImageUpload";
-import { ReactComponent as Back } from "../../assets/back.svg";
+import HashTag from "../../shared/HashTag";
 
 const BoardWrite = ({ boardName }) => {
   return (
     <>
-      <BoardHeader>
-        <Back />
-      </BoardHeader>
-      <ImageUpload />
+      <BoardWriteWrapper>
+        <ImageUpload />
+
+        <TextInputBox
+          height="48"
+          marginBtm="8"
+          placeholder={
+            boardName === "recipeBoard" ? "레시피 이름" : "게시글 제목"
+          }
+        />
+
+        {/* ------------------------ */}
+        {/* 레시피 작성시에만 렌더링 해줌 */}
+        {boardName === "recipeBoard" ? (
+          <TextInputBox height="48" marginBtm="8" placeholder="가격" />
+        ) : (
+          ""
+        )}
+        {/* ------------------------ */}
+
+        <TextInputBox
+          height="240"
+          marginBtm="16"
+          placeholder={
+            boardName === "recipeBoard"
+              ? "레시피 설명을 입력해주세요"
+              : "게시글 내용을 작성해주세요"
+          }
+        />
+
+        {/* ------------------------ */}
+        {/* 레시피 작성시에만 렌더링 해줌 */}
+        {boardName === "recipeBoard" && (
+          <>
+            <HashTagTitle>해시태그 선택</HashTagTitle>
+            <HashTag />
+            {/* ------------------------ */}
+          </>
+        )}
+      </BoardWriteWrapper>
     </>
   );
 };
 
-const BoardHeader = styled.div`
-  margin: 0px 16px 14px 16px;
+const BoardWriteWrapper = styled.div`
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TextInputBox = styled.textarea`
+  width: 320px;
+  height: ${(props) => props.height}px;
+  margin-bottom: ${(props) => props.marginBtm}px;
+  padding: 14px 16px;
+  background: #f8f8fa;
+  border-radius: 6px;
+`;
+
+const HashTagTitle = styled.p`
+  margin-bottom: 8px;
+  position: relative;
+  right: 35%;
+  font-size: 14px;
+  color: #999999;
 `;
 
 export default BoardWrite;
