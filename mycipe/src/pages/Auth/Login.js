e00790a5de40f794f2cbb047571935202765a02b
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-04 00:29:14 +0900

    [feat] : 이메일 로그인뷰 작성중(ing)

--- mycipe/src/pages/Auth/Login.js
+++ mycipe/src/pages/Auth/Login.js
@@ -1,13 +1,47 @@
 import React from "react";
 
+import styled from "styled-components";
 import { Button } from "../../elements";
+import { ReactComponent as Back } from "../../assets/back.svg";
 
 const Login = () => {
   return (
-    <>
-      <Button>로그인</Button>
-    </>
+    <React.Fragment>
+      <Header>
+        <Back />
+        로그인
+      </Header>
+      <InputId type="text" placeholder="이메일" />
+      <InputPwd type="text" placeholder="비밀번호" />
+      <Button margin="42px 20px 8px 20px">
+        <LoginText>로그인</LoginText>
+      </Button>
+    </React.Fragment>
   );
 };
 
 export default Login;
+
+const Header = styled.div`
+  display: flex;
+  height: 48px;
+`;
+
+const InputId = styled.input`
+  background-color: #f8f8fa;
+  width: 320px;
+  margin: 56px 20px 8px 20px;
+  height: 48px;
+  border-radius: 6px;
+`;
+const InputPwd = styled.input`
+  background-color: #f8f8fa;
+  width: 320px;
+  margin: 0px 20px 8px 20px;
+  height: 48px;
+  border-radius: 6px;
+`;
+
+const LoginText = styled.div`
+  color: white;
+`;
