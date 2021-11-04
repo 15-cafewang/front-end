784942f3ce0a1f0053794b3e7e60f74073882cc5
박하린 <phr0210@naver.com>
2021-11-04 19:04:08 +0900

    [edit] : 주석 추가

--- mycipe/src/elements/ProfileImage.js
+++ mycipe/src/elements/ProfileImage.js
@@ -17,14 +17,19 @@ ProfileImage.defaultProps = {
 };
 
 const ImageCircle = styled.div`
+  // 레시피 페이지, 게시판 페이지
   ${(props) =>
     props.size === "small"
       ? `width : 38px; height : 38px; border-radius : 38px;`
       : ""}
+
+  // 유저 페이지, 팔로잉(팔로우)목록 페이지
   ${(props) =>
     props.size === "medium"
       ? `width : 56px; height : 56px; border-radius : 56px;`
       : ""}
+      
+  // 프로필 편집 페이지
   ${(props) =>
     props.size === "large"
       ? `width : 80px; height : 80px; border-radius : 80px;`
