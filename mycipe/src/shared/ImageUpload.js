eb5d9f394b62fd273829454b3c308a86ff66949d
박하린 <phr0210@naver.com>
2021-11-04 13:17:06 +0900

    [edit] : handleChangeImageFile 함수 코드 수정

--- mycipe/src/shared/ImageUpload.js
+++ mycipe/src/shared/ImageUpload.js
@@ -9,17 +9,9 @@ const ImageUpload = () => {
   const [file, setFile] = useState({ file: "", previewURL: "" });
 
   const handleChangeImageFile = (e) => {
-    let fileReader = new FileReader(); // 파일데이터 자체를 불러오려면 FileRedaer API를 사용해야한다.
-    // 비동기적으로 데이터를 읽기 위해 읽을 파일을 가리키는 file 객체를 이용해 파일의 내용을 읽고 저장할 수 있다.
-
     let imageFile = e.target.files[0]; // 파일이 선택되었을 때 어떤 파일이 선택되었는지를 가리키는 객체
 
-    fileReader.readAsDataURL(imageFile); // 파일을 데이터 URL로 읽어온다. (파일을 읽는 방법 중 하나임)
-
-    // 파일 읽기가 성공적으로 완료되었을 때 실행
-    fileReader.onload = (e) => {
-      setFile({ file: imageFile, previewURL: e.target.result });
-    };
+    setFile({ file: imageFile, previewURL: URL.createObjectURL(imageFile) });
   };
 
   return (
