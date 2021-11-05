315b300398dcd5f115f1a86825edba6a18e1a1fd
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-05 20:33:03 +0900

    [feat] : 회원가입기능 추가

--- mycipe/src/pages/Auth/Signup.js
+++ mycipe/src/pages/Auth/Signup.js
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styled from "styled-components";
 import { Button } from "../../elements";
 import { ReactComponent as Back } from "../../assets/back.svg";
 
+import { useDispatch } from "react-redux";
+import { signUpDB } from "../../redux/Async/user";
+
 const Signup = () => {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onClickSignUp = () => {
+    const data = {
+      email: email,
+      password: password,
+      passwordCheck: passwordCheck,
+      nickname: nickname,
+    };
+    console.log(data);
+    dispatch(signUpDB(data));
+    console.log(data);
+  };
   return (
     <React.Fragment>
       <Header>
         <Back />
         회원가입
       </Header>
-      <InputId type="text" placeholder="이메일" />
-      <InputNick type="text" placeholder="닉네임" />
-      <InputPwd type="text" placeholder="비밀번호" />
-      <InputPwdChk type="text" placeholder="비밀번호 확인" />
-      <Button margin="32px 20px 8px 20px">
+      <InputId
+        type="text"
+        placeholder="이메일"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <InputNick
+        type="text"
+        placeholder="닉네임"
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <InputPwd
+        type="text"
+        placeholder="비밀번호"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <InputPwdChk
+        type="text"
+        placeholder="비밀번호 확인"
+        onChange={(e) => setPasswordCheck(e.target.value)}
+      />
+      <Button margin="32px 20px 8px 20px" _onClick={onClickSignUp}>
         <SignupText>계속하기</SignupText>
       </Button>
     </React.Fragment>
