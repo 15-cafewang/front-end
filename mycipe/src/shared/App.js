f4a592d22a1857c4274aabe384f650a9b8404e5c
박하린 <phr0210@naver.com>
2021-11-07 17:31:06 +0900

    [style] : Outter 컴포넌트 height값  수정

--- mycipe/src/shared/App.js
+++ mycipe/src/shared/App.js
@@ -108,14 +108,6 @@ const Outter = styled.div`
 
   background-size: cover;
   background-repeat: no-repeat;
-
-  /* background-color: red; */
-  @media screen and (max-height: 100vh) {
-    height: 100vh;
-  }
-  @media screen and (min-height: 100vh) {
-    height: auto;
-  }
 `;
 
 const WebVer = styled.div`
@@ -125,6 +117,7 @@ const WebVer = styled.div`
   z-index: -100;
   background: #ffe899;
   opacity: 0.18;
+  
   @media only screen and (min-width: 1025px) {
     opacity: 1;
     background-size: cover;
