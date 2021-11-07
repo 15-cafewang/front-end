3774edb8a9debfd5fb9418bd55be49b330c46ee0
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-08 01:33:43 +0900

    [feat] : pending 추가

--- mycipe/src/redux/Modules/userSlice.js
+++ mycipe/src/redux/Modules/userSlice.js
@@ -4,34 +4,62 @@ import { signupDB, loginDB, kakaoLogin } from "../Async/user";
 // inititalState
 const initialState = {
   isLogin: false,
-  userInfo: {},
+  isFetching: false,
+  userInfo: {
+    nickname: "",
+    profileImage: "image",
+  },
 };
 
 const userSlice = createSlice({
   name: "user",
+  isFetching: false,
   initialState: initialState,
   //리덕스
-  reducer: {
-    userReducer: (state, { payload }) => {
-      state.isLogin = payload;
-    },
-  },
+  reducers: {},
   extraReducers: {
+    // 회원가입
+    [signupDB.pending]: (state, ation) => {
+      state.isFetching = true;
+    },
     // 회원가입 성공시
     [signupDB.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
       window.alert("회원가입이 완료 되었습니다!");
     },
     // 회원가입 실패시
     [signupDB.rejected]: (state, action) => {
+      state.isFetching = false;
       window.alert("회원가입에 실패 하였습니다");
     },
+    // 로그인
+    [loginDB.pending]: (state, action) => {
+      state.isFetching = true;
+    },
+    // 로그인 성공시
     [loginDB.fulfilled]: (state, { payload }) => {
       state.userInfo = payload;
       state.isLogin = true;
       window.alert("로그인 되셨습니다! 환영합니다!");
     },
+    // 로그인 실패시
     [loginDB.rejected]: (state, action) => {
-      window.alert(action.meta.response.data.errMsg);
+      state.isFetching = false;
+      window.alert("로그인 실패");
+    },
+    //카카오
+    [kakaoLogin.pending]: (state, action) => {
+      state.isFetching = true;
+    },
+    // 카카오 로그인 성공시
+    [kakaoLogin.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
+      state.isLogin = true;
+      window.alert("로그인 되셨습니다! 환영합니다!");
+    },
+    [kakaoLogin.rejected]: (state, action) => {
+      state.isFetching = false;
+      window.alert("로그인 실패");
     },
   },
 });
