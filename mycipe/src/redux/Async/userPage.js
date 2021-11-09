8682f091fba5fd061203b21a8a1021b011eae260
shrkdvy <shrkdvy123@gmail.com>
2021-11-10 00:39:45 +0900

    [edit] : 유저페이지 thunk액션추가

--- mycipe/src/redux/Async/userPage.js
+++ mycipe/src/redux/Async/userPage.js
@@ -1,16 +1,68 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { history } from "../configureStore";
 
-import { getUserInfo } from "../../shared/api/userPageApi";
+//api
+import {
+  getUserInfo,
+  getUserWrittenRecipes,
+  getUserWrittenBoardes,
+  getUserLikeRecipes,
+  getUserLikeBoards,
+} from "../../shared/api/userPageApi";
 
+//user페이지 정보 불러오기
 const getUserInfoDB = createAsyncThunk(
   "userpage/userinfo",
   async (data, thunkAPI) => {
     const response = await getUserInfo(data);
-    console.log("asyncThunk 통신됨.");
 
-    return response.data;
+    return response.data.data;
   }
 );
 
-export { getUserInfoDB };
+//유저가 작성한 레시피 정보 불러오기
+const getUserWrittenRecipesDB = createAsyncThunk(
+  "userpage/recipes/write",
+  async (data, thunkAPI) => {
+    const response = await getUserWrittenRecipes(data);
+
+    return response.data.data.content;
+  }
+);
+
+//유저가작성한 게시물 정보 불러오기
+const getUserWrittenBoardsDB = createAsyncThunk(
+  "userpage/board/write",
+  async (data, thunkAPI) => {
+    const response = await getUserWrittenBoardes(data);
+
+    return response.data.data.content;
+  }
+);
+
+//유저가 좋아요한 레시피 정보 불러오기
+const getUserLikedRecipesDB = createAsyncThunk(
+  "userpage/recipes/like",
+  async (data, thunkAPI) => {
+    const response = await getUserLikeRecipes(data);
+
+    return response.data.data.content;
+  }
+);
+
+//유저가 좋아요한 게시물 정보 불러오기
+const getUserLikedBoardsDB = createAsyncThunk(
+  "userpage/board/like",
+  async (data, thunkAPI) => {
+    const response = await getUserLikeBoards(data);
+
+    return response.data.data.content;
+  }
+);
+
+export {
+  getUserInfoDB,
+  getUserWrittenRecipesDB,
+  getUserWrittenBoardsDB,
+  getUserLikedRecipesDB,
+  getUserLikedBoardsDB,
+};
