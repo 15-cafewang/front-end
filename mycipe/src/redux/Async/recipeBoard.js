f9cd815a798e8a421a0304a461842b5318944b2d
박하린 <phr0210@naver.com>
2021-11-09 10:07:10 +0900

    [feat] : 레시피 게시글 add thunk

--- mycipe/src/redux/Async/recipeBoard.js
+++ mycipe/src/redux/Async/recipeBoard.js
@@ -1,2 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import {} from "../";
+import { history } from "../configureStore";
+
+import { recipeBoardApi } from "../../shared/api/recipeBoardApi";
+
+// export const getListDB = createAsyncThunk("recipeBoard/getList",
+//   async (thunkAPI) => {
+//     const response = await getList();
+
+//   })
+
+export const addRecipePostDB = createAsyncThunk(
+  "recipeBoard/addPost",
+  async (data, thunkAPI) => {
+    console.log(data);
+    const response = await recipeBoardApi.addPost(data);
+    console.log(response);
+    window.alert(response.data.message);
+    history.push("/recipeboard");
+    return response.data.message;
+  }
+);
