41f16dd0647b1c4eb0c583feb9a3cd4dc074c8c0
박하린 <phr0210@naver.com>
2021-11-10 01:03:55 +0900

    [feat] : 레시피 목록 가져오기, 레시피 상세 조회 기능 추가

--- mycipe/src/redux/Async/recipeBoard.js
+++ mycipe/src/redux/Async/recipeBoard.js
@@ -3,18 +3,34 @@ import { history } from "../configureStore";
 
 import { recipeBoardApi } from "../../shared/api/recipeBoardApi";
 
-// export const getListDB = createAsyncThunk("recipeBoard/getList",
-//   async (thunkAPI) => {
-//     const response = await getList();
-
-//   })
-
+// 레시피 작성
 export const addRecipePostDB = createAsyncThunk(
   "recipeBoard/addPost",
   async (data, thunkAPI) => {
     const response = await recipeBoardApi.addPost(data);
-    window.alert(response.data.message);
+    // window.alert(response.data.message);
     history.push("/recipeboard");
     return response.data.message;
+  }
+);
+
+// 레시피 목록 가져오기
+export const getRecipePostListDB = createAsyncThunk(
+  "recipeBoard/getPostList",
+  async (thunkAPI) => {
+    const response = await recipeBoardApi.getPostList();
+    // window.alert(response.data.message);
+    history.push("/recipeboard");
+    return response.data.data.content;
+  }
+);
+
+// 레시피 상세 조회
+export const getRecipePostDetailDB = createAsyncThunk(
+  "recipeBoard/getPostDetail",
+  async (data, thunkAPI) => {
+    const response = await recipeBoardApi.getPostDetail(data);
+    // window.alert(response.data.message);
+    return response.data.data;
   }
 );
