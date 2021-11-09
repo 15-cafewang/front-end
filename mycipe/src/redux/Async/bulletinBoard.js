9366daf5f2fcfd57c1709a92535a84fa65b48150
박하린 <phr0210@naver.com>
2021-11-10 01:03:13 +0900

    [feat] : 게시글 목록가져오기, 게시글 상세 조회 기능 추가

--- mycipe/src/redux/Async/bulletinBoard.js
+++ mycipe/src/redux/Async/bulletinBoard.js
@@ -9,21 +9,33 @@ import { bulletinBoardApi } from "../../shared/api/bulletinBoardApi";
 
 //   })
 
+// 게시글 작성
 export const addBulletinPostDB = createAsyncThunk(
   "bulletinBoard/addPost",
   async (data, thunkAPI) => {
     const response = await bulletinBoardApi.addPost(data);
-    window.alert(response.data.message);
+    // window.alert(response.data.message);
     history.push("/bulletinboard");
     return response.data.message;
   }
 );
 
-export const getBulletinBoardListDB = createAsyncThunk(
+// 게시판 목록 가져오기
+export const getBulletinPostListDB = createAsyncThunk(
   "bulletinBoard/getPostList",
   async (thunkAPI) => {
     const response = await bulletinBoardApi.getList();
-    window.alert(response.data.message);
-    return response.data.content;
+    // window.alert(response.data.message);
+    return response.data.data.content;
+  }
+);
+
+// 게시판 상세 조회
+export const getBulletinPostDetailDB = createAsyncThunk(
+  "recipeBoard/getPostDetail",
+  async (data, thunkAPI) => {
+    const response = await bulletinBoardApi.getPostDetail(data);
+    // window.alert(response.data.message);
+    return response.data.data;
   }
 );
