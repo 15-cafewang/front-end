27e67f1f9307a496ddbcfb2bafa88b2b02a953b6
shrkdvy <shrkdvy123@gmail.com>
2021-11-09 22:42:21 +0900

    [edit] : image 추가

--- mycipe/src/elements/index.js
+++ mycipe/src/elements/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import BigFilterButton from "./BigFilterButton";
 import SmallFilterButton from "./SmallFilterButton";
 import ButtonInner from "./ButtonInner";
+import Image from "./Image";
 
-export { Button, BigFilterButton, ButtonInner, SmallFilterButton };
+export { Button, BigFilterButton, ButtonInner, SmallFilterButton, Image };
