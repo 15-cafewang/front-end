bab6726e558528a58542cf80b9106752eaccb089
박하린 <phr0210@naver.com>
2021-11-10 01:00:26 +0900

    [edit] : 서버로부터 레시피목록 받아와서 recipecard 컴포넌트에 props로 전달 하도록 수정

--- mycipe/src/pages/RecipeBoard/RecipeBoardMain.js
+++ mycipe/src/pages/RecipeBoard/RecipeBoardMain.js
@@ -1,13 +1,22 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import RecipeCard from "../../components/Card/RecipeCard";
 import ModalBackground from "../../shared/ModalBackground";
+import { getRecipePostListDB } from "../../redux/Async/recipeBoard";
+import { history } from "../../redux/configureStore";
 
 const RecipeBoardMain = () => {
+  const dispatch = useDispatch();
   const isActive = useSelector((state) => state.modal.isActive);
-  const src = "";
+  const recipeList = useSelector(
+    (state) => state.recipeBoard && state.recipeBoard.recipeList
+  );
+
+  useEffect(() => {
+    dispatch(getRecipePostListDB());
+  }, []);
 
   return (
     <>
@@ -18,13 +27,33 @@ const RecipeBoardMain = () => {
           <SortingItem>최신순</SortingItem>
           <SortingItem>인기순</SortingItem>
         </SortingBox>
-
+        {/* commentCount: 0 content: "dd" images: [] likeCount: 0 likeStatus: false
+        nickname: "박하린" price: 22 recipeId: 46 regdate:
+        "2021-11-09T18:08:32.114896" title: "dd" */}
         <CardList>
-          <RecipeCard src={src} />
-          <RecipeCard src={src} />
-          <RecipeCard src={src} />
-          <RecipeCard src={src} />
-          <RecipeCard src={src} />
+          {recipeList &&
+            recipeList.map((r, idx) => {
+              return (
+                <RecipeCard
+                  _onClick={() => {
+                    history.push(`/recipeboard/detail/${r.recipeId}`);
+                  }}
+                  key={r.recipeId}
+                  commentCount={r.commentCount}
+                  content={r.content}
+                  image={r.images[0]}
+                  likeCount={r.likeCount}
+                  likeStatus={r.likeStatus}
+                  nickname={r.nickname}
+                  price={r.price}
+                  regDate={r.regdate
+                    .split("T")[0]
+                    .replace("-", ". ")
+                    .replace("-", ". ")}
+                  title={r.title}
+                />
+              );
+            })}
         </CardList>
       </BoardMainContainer>
     </>
