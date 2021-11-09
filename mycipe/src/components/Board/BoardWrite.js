24d94d4bdbc219144e16e28b37140b2e2b962a5e
박하린 <phr0210@naver.com>
2021-11-09 10:05:41 +0900

    [feat] : 게시글 add 기능 추가

--- mycipe/src/components/Board/BoardWrite.js
+++ mycipe/src/components/Board/BoardWrite.js
@@ -1,29 +1,93 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+/* eslint-disable array-callback-return */
+import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
-
-// shard components
+import { history } from "../../redux/configureStore";
+// icon
+import { ReactComponent as BackIcon } from "../../assets/icon/HeaderIcon/back.svg";
+// shared components
 import ImageListUpload from "../../shared/ImageListUpload";
 import HashTag from "../../shared/HashTag";
 import ModalBackground from "../../shared/ModalBackground";
-
+// async function
+import { addRecipePostDB } from "../../redux/Async/recipeBoard";
+import { addBulletinPostDB } from "../../redux/Async/bulletinBoard";
 const BoardWrite = ({ boardName }) => {
   const dispatch = useDispatch();
   const isActive = useSelector((state) => state.modal.isActive);
-  const [post, setPost] = useState({ title: "", price: "", content: "" });
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [content, setContent] = useState("");
+  const [fileList, setFileList] = useState([]);
+  const [tagList, setTagList] = useState([]);
 
-  const addPost = (post) => {
-    dispatch();
+  // fileList를 받아와서 setFile 해줍니다.
+  const getFileFromImageList = (fileList) => {
+    setFileList([...fileList]);
+  };
+
+  // tagList를 받아와서 setTag 해줍니다.
+  const getTagFromHashTag = (tagList) => {
+    setTagList(tagList);
+  };
+
+  const addPost = () => {
+    // 레시피 폼데이터
+    const recipeFormData = new FormData();
+    recipeFormData.append("title", title);
+    recipeFormData.append("content", content);
+    recipeFormData.append("price", price);
+    recipeFormData.append("tag", tagList);
+    for (const f of fileList) {
+      recipeFormData.append("image", f);
+    }
+
+    // 게시판 폼데이터
+    const bulletinFormData = new FormData();
+    bulletinFormData.append("title", title);
+    bulletinFormData.append("content", content);
+    for (const f of fileList) {
+      bulletinFormData.append("image", f);
+    }
+
+    if (boardName === "recipeBoard") {
+      dispatch(addRecipePostDB(recipeFormData));
+    }
+    if (boardName === "bulletinBoard") {
+      dispatch(addBulletinPostDB(bulletinFormData));
+    }
   };
 
   return (
     <>
+      <HeaderInner flexBetween>
+        <LeftInner>
+          <BackIcon
+            onClick={() => {
+              history.goBack();
+            }}
+          />
+          <PageName>
+            {boardName === "recipeBoard"
+              ? "레시피 작성하기"
+              : "게시글 작성하기"}
+          </PageName>
+        </LeftInner>
+
+        <Button
+          onClick={() => {
+            addPost();
+          }}
+        >
+          완료
+        </Button>
+      </HeaderInner>
       <BoardWriteWrapper>
         {isActive && <ModalBackground />}
-        <ImageListUpload />
+        <ImageListUpload getFileFromImageList={getFileFromImageList} />
 
         <TextInputBox
-          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          onChange={(e) => setTitle(e.target.value)}
           height="48"
           marginBtm="8"
           placeholder={
@@ -34,7 +98,7 @@ const BoardWrite = ({ boardName }) => {
         {/* 레시피 작성시에만 렌더링 해줌 */}
         {boardName === "recipeBoard" ? (
           <TextInputBox
-            onChange={(e) => setPost({ ...post, price: e.target.value })}
+            onChange={(e) => setPrice(1 * e.target.value)}
             height="48"
             marginBtm="8"
             placeholder="가격"
@@ -44,7 +108,7 @@ const BoardWrite = ({ boardName }) => {
         )}
 
         <TextInputBox
-          onChange={(e) => setPost({ ...post, content: e.target.value })}
+          onChange={(e) => setContent(e.target.value)}
           height="240"
           marginBtm="16"
           placeholder={
@@ -58,18 +122,9 @@ const BoardWrite = ({ boardName }) => {
         {boardName === "recipeBoard" && (
           <>
             <HashTagTitle>해시태그 선택</HashTagTitle>
-            <HashTag />
+            <HashTag getTagFromHashTag={getTagFromHashTag} />
           </>
         )}
-
-        <button
-          onClick={() => {
-            // console.log(post);
-            addPost(post);
-          }}
-        >
-          완료
-        </button>
       </BoardWriteWrapper>
     </>
   );
@@ -83,6 +138,42 @@ const BoardWriteWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const HeaderInner = styled.div`
+  width: 100%;
+  height: 48px;
+  z-index: 1;
+  padding: 0px 20px;
+  position: sticky;
+  top: 0;
+
+  background: #fff;
+  display: flex;
+  align-items: center;
+  ${(props) =>
+    props.flexBetween &&
+    css`
+      justify-content: space-between;
+    `}
+`;
+
+const PageName = styled.span`
+  font-size: 16px;
+  margin-left: 8px;
+`;
+
+const LeftInner = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  color: #7692e4;
+  justify-content: center;
 `;
 
 const TextInputBox = styled.textarea`
