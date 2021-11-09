87b04823f0438b720fbaa3e343c4f3de5d3357f4
박하린 <phr0210@naver.com>
2021-11-09 10:08:30 +0900

    [feat] : 레시피 게시글 add reducer

--- mycipe/src/redux/Modules/recipeBoardSlice.js
+++ mycipe/src/redux/Modules/recipeBoardSlice.js
@@ -1,8 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// async import
+import { addRecipePostDB } from "../Async/recipeBoard";
 
 // initialstate
 const initialstate = {
+  isfetching: false,
+};
 
-}
+const recipeBoardSlice = createSlice({
+  name: "recipeBoard",
+  initialState: initialstate,
+  reducers: {},
+  extraReducers: {
+    // 레시피 작성
+    [addRecipePostDB.pending]: (state, action) => {
+      state.isfetching = true;
+    },
+    [addRecipePostDB.fulfilled]: (state, action) => {
+      state.isfetching = false;
+    },
+    [addRecipePostDB.fulfilled]: (state, action) => {
+      state.isfetching = true;
+    },
+  },
+});
+
+export default recipeBoardSlice;
