3f26a94f1e74c237eccb207c3a85c4f87e36c9b6
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-10 12:27:31 +0900

    [edit] : 헤더부분 코드 수정

--- mycipe/src/shared/Header.js
+++ mycipe/src/shared/Header.js
@@ -19,39 +19,13 @@ const Header = (props) => {
 
   if (
     location === "/" ||
+    location === "/signup" ||
+    location === "/login" ||
     location === "/recipeboard/write" ||
     location === "/bulletinboard/write"
   ) {
     // 시작페이지
     return null;
-  }
-
-  if (location === "/signup") {
-    // 회원가입페이지
-    return (
-      <HeaderInner>
-        <BackIcon
-          onClick={() => {
-            history.goBack();
-          }}
-        />
-        <PageName>회원가입</PageName>
-      </HeaderInner>
-    );
-  }
-
-  if (location === "/login") {
-    // 로그인페이지
-    return (
-      <HeaderInner>
-        <BackIcon
-          onClick={() => {
-            history.goBack();
-          }}
-        />
-        <PageName>로그인</PageName>
-      </HeaderInner>
-    );
   }
 
   if (location === "/main") {
