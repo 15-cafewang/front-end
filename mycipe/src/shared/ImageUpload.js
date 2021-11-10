af2e40fcf552582a2af7efb7a8d84109920bfa83
shrkdvy <shrkdvy123@gmail.com>
2021-11-10 15:06:21 +0900

    [edit] : 업로드파일 확장자 수정 , 업로드 취소시 화면에 보이기

--- mycipe/src/shared/ImageUpload.js
+++ mycipe/src/shared/ImageUpload.js
@@ -2,59 +2,56 @@ import { React, useState } from "react";
 import styled from "styled-components";
 
 // icon
-import { ReactComponent as PlusImageBefore } from "../assets/plusImageBefore.svg";
-import { ReactComponent as PlusImageAfter } from "../assets/plusImageAfter.svg";
 
 // elements
 import Image from "../elements/Image";
 
-const ImageUpload = () => {
-  const [file, setFile] = useState({ file: "", previewURL: "" });
+import { ReactComponent as ProfileEditIcon } from "../assets/icon/profileEdit.svg";
+
+const ImageUpload = ({ file, setFile, profileImage }) => {
+  let currentSelectedFile = file.file;
+  let currentSelectedImage = profileImage;
+  if (file.previewURL) currentSelectedImage = file.previewURL;
 
   const handleChangeImageFile = (e) => {
     let imageFile = e.target.files[0]; // 파일이 선택되었을 때 어떤 파일이 선택되었는지를 가리키는 객체
 
     setFile({
-      file: imageFile,
-      previewURL: imageFile ? URL.createObjectURL(imageFile) : "",
+      file: imageFile ? imageFile : currentSelectedFile,
+      previewURL: imageFile
+        ? URL.createObjectURL(imageFile)
+        : currentSelectedImage,
     });
   };
 
   return (
     <>
-      <Image
-        shape="rectangle"
-        size="large"
-        src={file.previewURL ? file.previewURL : null}
-      >
-        <label>
-          {/* 사진 선택 유뮤에 따라 아이콘 색상 변경 */}
-          {file.previewURL ? (
-            <IconWrapper>
-              <PlusImageAfter />
-            </IconWrapper>
-          ) : (
-            <IconWrapper>
-              <PlusImageBefore />
-            </IconWrapper>
-          )}
-          <InputFile
-            type="file"
-            accept="image/jpg, impge/png, image/jpeg, image/gif"
-            onChange={handleChangeImageFile}
-          ></InputFile>
-        </label>
-      </Image>
+      <LabelBox>
+        <Image shape="circle" size="large" src={currentSelectedImage} />
+        {/* 플러스아이콘 */}
+        <EditButtonInner>
+          <ProfileEditIcon />
+        </EditButtonInner>
+        <InputFile
+          type="file"
+          // accept="image/jpg, impge/png, image/jpeg, image/gif"
+          accept="image/*"
+          onChange={handleChangeImageFile}
+        />
+      </LabelBox>
     </>
   );
 };
 
-const IconWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const LabelBox = styled.label`
+  cursor: pointer;
+`;
+
+const EditButtonInner = styled.div`
+  position: absolute;
+
+  right: 0;
+  bottom: 0;
 `;
 
 const InputFile = styled.input`
