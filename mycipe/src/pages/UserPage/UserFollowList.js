da924f8be772bf5fe413046018f3c20773f3fa33
shrkdvy <shrkdvy123@gmail.com>
2021-11-12 16:38:40 +0900

    [edit] : 팔로워,팔로우 리스트 페이지

--- mycipe/src/pages/UserPage/UserFollowList.js
+++ mycipe/src/pages/UserPage/UserFollowList.js
@@ -1,30 +1,96 @@
 import React from "react";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { BigFilterButton } from "../../elements";
 
 import UserCard from "../../components/Card/UserCard";
+
 import ModalBackground from "../../shared/ModalBackground";
 
+import { useParams } from "react-router-dom";
+
+import {
+  userFollowingListDB,
+  userFollowListDB,
+} from "../../redux/Async/userPage";
+
 const UserPageFollowList = (props) => {
+  const dispatch = useDispatch();
+  const nickname = useParams().nickname;
+
   const isActive = useSelector((state) => state.modal.isActive);
+  const userList = useSelector((state) => state.userPage.userList);
+  const isFollower = useSelector((state) => state.userPage.isFollower);
 
   return (
     <>
       <Container>
         {isActive && <ModalBackground />}
-        <BigFilterButton active noneBorderTop>
+        <BigFilterButton
+          active={isFollower}
+          noneBorderTop
+          _onClick={() => {
+            dispatch(userFollowListDB(nickname));
+          }}
+        >
           팔로워
         </BigFilterButton>
-        <BigFilterButton noneBorderTop>팔로잉</BigFilterButton>
+        <BigFilterButton
+          active={!isFollower}
+          noneBorderTop
+          _onClick={() => {
+            dispatch(userFollowingListDB(nickname));
+          }}
+        >
+          팔로잉
+        </BigFilterButton>
 
         <UserCardList>
-          <UserCard />
-          <UserCard />
-          <UserCard />
-          <UserCard />
-          <UserCard />
+          {userList.map((item, idx) => {
+            return <UserCard key={idx} {...item} />;
+          })}
         </UserCardList>
+
+        {/* 추가 기능 사항 */}
+        {/* {nickname === LoginUserNickname ? (
+          <UserCardList>
+            {userList.map((item, idx) => {
+              if (isFollower) return <FollowerCard key={idx} {...item} />;
+              else
+                return (
+                  <FollowingCard key={idx} {...item} followingState={true} />
+                );
+
+       
+            })}
+          </UserCardList>
+        ) : (
+          <UserCardList>
+            {userList.map((item, idx) => {
+              if (isFollower) {
+                if (LoginUserList.includes(item.nickname)) {
+                  return (
+                    <FollowingCard key={idx} {...item} followingState={true} />
+                  );
+                } else {
+                  return (
+                    <FollowingCard key={idx} {...item} followingState={false} />
+                  );
+                }
+              } else {
+                if (LoginUserList.includes(item.nickname)) {
+                  return (
+                    <FollowingCard key={idx} {...item} followingState={true} />
+                  );
+                } else {
+                  return (
+                    <FollowingCard key={idx} {...item} followingState={false} />
+                  );
+                }
+              }
+            })}
+          </UserCardList>
+        )} */}
       </Container>
     </>
   );
