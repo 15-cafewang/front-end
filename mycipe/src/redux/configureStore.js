d0dc1ab645b5de892d849256ac9ed02511fe316a
박하린 <phr0210@naver.com>
2021-11-17 16:57:08 +0900

    [fix] : 미들웨어 serializable error fix

--- mycipe/src/redux/configureStore.js
+++ mycipe/src/redux/configureStore.js
@@ -42,9 +42,13 @@ if (env === "development") {
   middlewares.push(logger);
 }
 
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
+
 const store = configureStore({
   reducer: reducer,
-  middleware: [...middlewares, ...getDefaultMiddleware()],
+  middleware: [...middlewares, ...customizedMiddleware],
   devTools: env !== "production",
 });
 
