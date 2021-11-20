034cd61b040e4d131b34b0b3351375a704c6b322
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-11-20 15:56:01 +0900

    [feat]:스피너추가

--- mycipe/src/components/Kakao.js
+++ mycipe/src/components/Kakao.js
@@ -1,6 +1,8 @@
 import React from "react";
+import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { kakaoLogin } from "../redux/Async/user";
+import Spinner from "../assets/image/Spinner.gif";
 
 const Kakao = (props) => {
   const dispatch = useDispatch();
@@ -11,10 +13,18 @@ const Kakao = (props) => {
     dispatch(kakaoLogin(code));
   }, []);
   return (
-    <>
-      <div>로딩중</div>;
-    </>
+    <SpinnerBox>
+      <SpinnerImg src={Spinner} />
+    </SpinnerBox>
   );
 };
 
 export default Kakao;
+
+const SpinnerBox = styled.div`
+  text-align: center;
+`;
+
+const SpinnerImg = styled.img`
+  margin-top: 25vh;
+`;
