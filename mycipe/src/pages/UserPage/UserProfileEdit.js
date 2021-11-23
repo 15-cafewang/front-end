79e957263ecd7d627eb3f191c61217938dea6cc2
shrkdvy <shrkdvy123@gmail.com>
2021-11-23 11:24:59 +0900

    [edit] : alert창 추가

--- mycipe/src/pages/UserPage/UserProfileEdit.js
+++ mycipe/src/pages/UserPage/UserProfileEdit.js
@@ -6,6 +6,8 @@ import { history } from "../../redux/configureStore";
 import _ from "lodash";
 
 import ModalBackground from "../../shared/ModalBackground";
+import PopUp from "../../shared/PopUp";
+
 import { nickCheck as validNickname } from "../../shared/common";
 import { Button, Text } from "../../elements/";
 
@@ -16,7 +18,7 @@ import { updateUserInfoDB } from "../../redux/Async/user";
 import { nicknameCheckAPI as confirmNicknameAPI } from "../../shared/api/userApi";
 import { updateUserInfo } from "../../redux/Modules/userSlice";
 
-const UserpageProfileEdit = (props) => {
+const UserpageProfileEdit = () => {
   const dispatch = useDispatch();
   const isActive = useSelector((state) => state.modal.isActive);
   const LoginUserInfo = useSelector((state) => state.user.userInfo);
@@ -25,6 +27,23 @@ const UserpageProfileEdit = (props) => {
     file: "",
     previewURL: LoginUserInfo.profileImage,
   });
+
+  // alert
+  const [popUp, setPopUp] = useState(null);
+  const [message, setMessage] = useState("");
+  const [isConfirm, setIsConfirm] = useState(false);
+
+  const alertPopUp = (message, delay, newNickname = null) => {
+    setPopUp(true);
+    setMessage(message);
+
+    setTimeout(() => {
+      setPopUp(false);
+      newNickname && history.push(`/usermain/${newNickname}`);
+    }, delay);
+  };
+
+  //새로운 닉네임 input
   const inputRef = useRef();
 
   //유저정보 변경
@@ -39,22 +58,19 @@ const UserpageProfileEdit = (props) => {
         profileFofmData.append("image", file.file);
       }
 
-      const response = await dispatch(
-        updateUserInfoDB(profileFofmData)
-      ).unwrap();
-
-      if (response.code === 1) {
-        dispatch(
-          updateUserInfo({
-            nickname: newNickname,
-            profileImage: file.previewURL,
-          })
-        );
-      }
-
-      history.push(`/usermain/${newNickname}`);
-    } catch (error) {
-      console.log(error);
+      const msg = await dispatch(updateUserInfoDB(profileFofmData)).unwrap();
+      console.log(msg);
+      dispatch(
+        updateUserInfo({
+          nickname: newNickname,
+          profileImage: file.previewURL,
+        })
+      );
+      //alert
+      alertPopUp(msg, 700, newNickname);
+    } catch (errorMsg) {
+      //alert
+      alertPopUp(errorMsg, 1000);
     }
   };
 
@@ -66,18 +82,29 @@ const UserpageProfileEdit = (props) => {
 
   //중복확인
   const confirmNickname = async () => {
-    const response = await confirmNicknameAPI(inputRef.current.value);
-
-    if (response.data.code === 1) window.alert(response.data.message);
-    else window.alert(response.data.message);
+    try {
+      const response = await confirmNicknameAPI(inputRef.current.value);
+      setIsConfirm(true);
+      alertPopUp(response.data.message, 700);
+    } catch (error) {
+      alertPopUp(error.data.message, 700);
+    }
   };
 
   return (
     <ProfileInfoInner>
       {isActive && <ModalBackground />}
+      {/* alert 창 */}
+      <PopUp
+        popUp={popUp}
+        setPopUp={setPopUp}
+        message={message}
+        isButton={false}
+      />
+
       <UserProfileImageInner onClick={() => {}}>
         <ImageUpload
-          profileImage={file.previewURL}
+          profileImage={LoginUserInfo.profileImage}
           file={file}
           setFile={setFile}
         />
@@ -106,7 +133,14 @@ const UserpageProfileEdit = (props) => {
           </Text>
         )}
       </Grid>
-      <Button color="#fff" _onClick={updateInfo}>
+
+      <Button
+        color="#fff"
+        _onClick={() => {
+          if (isConfirm) updateInfo();
+          else alertPopUp("중복확인을 해주세요", 700);
+        }}
+      >
         변경하기
       </Button>
     </ProfileInfoInner>
