557a6989e6a39d604f7f1f96ddda9e973a475eb1
shrkdvy <shrkdvy123@gmail.com>
2021-11-25 14:32:34 +0900

    [edit] : main에 랭킹유저 카드추가

--- mycipe/src/components/Card/UserCard.js
+++ mycipe/src/components/Card/UserCard.js
@@ -1,34 +1,78 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Image } from "../../elements/index.js";
 
 import { history } from "../../redux/configureStore";
 
-const UserCard = ({ nickname, image }) => {
-  return (
-    <>
-      <UserCardInner between>
-        <Grid>
-          <Image
-            shape="circle"
-            size="medium"
-            src={image}
-            _onClick={() => {
-              history.push(`/usermain/${nickname}`);
-            }}
-          />
-          <Text>{nickname}</Text>
-        </Grid>
-      </UserCardInner>
-    </>
-  );
+const UserCard = ({
+  nickname = "노강표",
+  image,
+  isrank = false,
+  rank = 0,
+  category = "팔로우",
+  count = 100,
+}) => {
+  if (isrank) {
+    return (
+      <>
+        <UserCardInner between margin="16px 0px">
+          <Grid>
+            <Text margin="0px 17px 0px 9px" size="20px">
+              {rank}
+            </Text>
+            <Image
+              shape="circle"
+              size="medium"
+              src={image}
+              _onClick={() => {
+                history.push(`/usermain/${nickname}`);
+              }}
+            />
+
+            <GridRight margin="0px 0px 0px 20px">
+              <Text size="14px">{nickname}</Text>
+              <GridRight>
+                <Text size="14px" color="#999">
+                  {category} 수
+                </Text>
+                <Text size="14px" margin="0px 0px 0px 9px">
+                  {count}개
+                </Text>
+              </GridRight>
+            </GridRight>
+          </Grid>
+        </UserCardInner>
+        <Line />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <UserCardInner between>
+          <Grid>
+            <Image
+              shape="circle"
+              size="medium"
+              src={image}
+              _onClick={() => {
+                history.push(`/usermain/${nickname}`);
+              }}
+            />
+            <Text size="16px" margin="0px 0px 0px 20px">
+              {nickname}
+            </Text>
+          </Grid>
+        </UserCardInner>
+      </>
+    );
+  }
 };
 
 const UserCardInner = styled.li`
   display: flex;
   justify-content: ${(props) =>
     props.between ? "space-between" : "flex-start"};
-  margin: 20px;
+  margin: ${(props) => (props.margin ? props.margin : "")};
   align-items: center;
 `;
 
@@ -37,8 +81,20 @@ const Grid = styled.div`
   align-items: center;
 `;
 
+const GridRight = styled.div`
+  margin: ${(props) => (props.margin ? props.margin : "")};
+`;
+
 const Text = styled.span`
-  margin-left: 20px;
+  color: ${(props) => (props.color ? props.color : "#000")};
+  font-size: ${(props) => (props.size ? props.size : "")};
+  margin: ${(props) => (props.margin ? props.margin : "")};
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #ededed;
 `;
 
 // const FollowingButton = styled.button`
