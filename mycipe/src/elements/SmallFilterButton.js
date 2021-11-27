d51eeed19d8304073048579eeae4f543b0766f37
shrkdvy <shrkdvy123@gmail.com>
2021-11-27 17:20:55 +0900

    [edit] : padding props 설정

--- mycipe/src/elements/SmallFilterButton.js
+++ mycipe/src/elements/SmallFilterButton.js
@@ -2,9 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const SmallFilterButton = (props) => {
-  const { children, active, _onClick } = props;
+  const { children, active, _onClick, padding } = props;
 
-  const styles = { active };
+  const styles = { active, padding };
 
   return (
     <Button {...styles} onClick={_onClick}>
@@ -19,7 +19,8 @@ SmallFilterButton.defalutProps = {
 };
 
 const Button = styled.button`
-  padding: 8px;
+  /* padding: 8px; */
+  padding: ${(props) => (props.padding ? props.padding : "8px")};
   font-size: 12px;
   color: #767676;
   ${(props) => props.active && `color : #000; font-weight : 500`};
