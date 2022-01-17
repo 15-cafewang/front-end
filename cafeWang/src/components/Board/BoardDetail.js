d5e4ddbaafddd0ecd07a5ae40512837678f40271
shrkdvy <shrkdvy123@gmail.com>
2022-01-17 09:53:58 +0900

    [edit} : 추가 댓글 작성시 팝업창이 안나오는 오류 해결

--- cafeWang/src/components/Board/BoardDetail.js
+++ cafeWang/src/components/Board/BoardDetail.js
@@ -188,6 +188,7 @@ const BoardDetail = ({ boardName }) => {
   const addComment = () => {
     if (!content) {
       alertPopUp(" 내용을 작성해 주세요!", 1200);
+      setButtonName(null);
       return;
     }
 
